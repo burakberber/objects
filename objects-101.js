8dc551088dd70bb8569d9639f540e5e2a00f8b0e
burakberber <burak@burakberber.net>
2020-05-18 03:46:05 +0300

    Add files via upload

---
+++ objects-101.js
@@ -0,0 +1,21 @@
+let myBook = {
+    title: '1984',
+    author: 'brbrbrk',
+    pageCount: 326
+}
+console.log(myBook.title)
+console.log(''+myBook.title+' by '+myBook.author+'')
+//console.log('${myBook.title} by ${myBook.author}')
+myBook.title = 'thisIsnew'
+console.log(''+myBook.title+' by '+myBook.author+'')
+
+//hey burak
+
+let me={
+    name: 'burak',
+    age: 27,
+    location: 'Philadelphia'
+}
+console.log(''+me.name+'  is '+me.age+' and lives in '+me.location+'')
+let agee=me.age+1
+console.log(agee)
